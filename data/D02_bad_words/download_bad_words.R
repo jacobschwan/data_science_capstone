39b37613650397df356c6449032ceacf97224517
Jacob Schwan <blackbrass88@gmail.com>
2018-09-16 13:40:12 -0500

    add bad words data set

---
+++ data/D02_bad_words/download_bad_words.R
@@ -0,0 +1,8 @@
+# Download the [List of Dirty, Naughty, Obscene, and Otherwise Bad Words](https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/blob/master/README.md)
+# provided by [Shutterstock](http://www.shuttertock.com) in a GitHub repository.
+
+pacman::p_load(here)
+
+download.file("https://raw.githubusercontent.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en",
+              here("data/D02_bad_words/bad_words.txt"))
+
